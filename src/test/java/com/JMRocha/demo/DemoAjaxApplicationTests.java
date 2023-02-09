0c6675256e6ca74ff89c54fd370a336dae4ba2f4
Juliano Rocha <julianorocha.ti@gmail.com>
2023-02-09 18:44:10 -0300

    Projeto criado em ajax

---
+++ src/test/java/com/JMRocha/demo/DemoAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JMRocha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

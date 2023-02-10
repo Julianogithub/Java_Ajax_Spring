c5c9ec4af65a7a3dd6d0dec79dfd73c935329fa1
Juliano Rocha <julianorocha.ti@gmail.com>
2023-02-09 23:42:46 -0300

    Usando o JSoup e testando os recursos da OpenGraph

--- src/main/java/com/JMRocha/demo/DemoAjaxApplication.java
+++ src/main/java/com/JMRocha/demo/DemoAjaxApplication.java
@@ -1,13 +1,29 @@
 package com.JMRocha.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.JMRocha.demo.model.SocialMetaTag;
+import com.JMRocha.demo.service.SocialMetaTagService;
+
 @SpringBootApplication
-public class DemoAjaxApplication {
+public class DemoAjaxApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+	
+	@Autowired
+	SocialMetaTagService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SocialMetaTag og = service.getSocialMetaTagByUrl("https://naturalmed.com.br/produto/curcuma-plus-60-caps-vitafor/");
+		System.out.println("\n\t"+og.toString()+"\n");
+		
 	}
 
 }
---
+++ src/main/java/com/JMRocha/demo/service/SocialMetaTagService.java
@@ -0,0 +1,32 @@
+package com.JMRocha.demo.service;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
+import com.JMRocha.demo.model.SocialMetaTag;
+
+@Service
+
+public class SocialMetaTagService {
+	
+	public SocialMetaTag getSocialMetaTagByUrl(String url) {
+
+		SocialMetaTag tag = new SocialMetaTag();
+		try {
+			
+			Document doc = Jsoup.connect(url).get();
+			tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
+			tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
+			tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+			tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+		
+		} catch (Exception e) {
+			e.printStackTrace();	
+		}	
+		return tag;
+	}
+	
+
+}

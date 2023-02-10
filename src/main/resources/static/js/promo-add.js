75ebc5598d03e99e88557849d09a303cff039bc2
Juliano Rocha <julianorocha.ti@gmail.com>
2023-02-10 10:58:58 -0300

    Carregando os dados de retorno no Formulario

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,12 +10,15 @@ $("#linkPromocao").on('change', function() {
 		$.ajax({
 			method:"POST", // Recuperando o valo do tipo " Post na classes Java controlle" --> @PostMapping("/info")
 			url: "/meta/info?url=" + url, // Capiturando ocaminho da URL na classes Java para exibição na pagina WEB
-			cache: false, //Não fazer o uso do cache
+			cache: false,//Não fazer o uso do cache
 			
 			success: function( data ) {
 				console.log(data);
+				//comecção MVC com o --> * HTML, JAVA, SQL *
+				$("#titulo").val(data.title);//Para a pagina HTML--> $("#titulo") || Busca pela a classe Java-->val(data.title);
+				$("#site").text(data.site.replace("@", ""));
+				$("#linkImagem").attr("src", data.image);
 			}
-			
 		});
 	}
 });
